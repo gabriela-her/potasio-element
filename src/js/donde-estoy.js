3db1d8304ac36c2e89bc9157574743b97496f4c9
irinatiron <irinatirongiurgiu@gmail.com>
2025-06-20 15:19:21 +0200

    added h5

--- src/js/donde-estoy.js
+++ src/js/donde-estoy.js
@@ -1,14 +1,14 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex); // Cuando se muestra el corousel se sitúa en su index que se define como 1, la primera fruta
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n); // Se mueve hacia el item anterior o al siguiente
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(slideIndex = n); // Se mueve hacia el item con número n
 }
 
 function showSlides(n) {
@@ -18,11 +18,11 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; // Sólo muestra un carousel-item y oculta los demás
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" carousel-item-active", "");
+    dots[i].className = dots[i].className.replace(" carousel-item-active", ""); // Muestra en color verde para resaltar solo el doc del carousel-item que está activo
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " carousel-item-active";
+  dots[slideIndex-1].className += " carousel-item-active"; // Añade la clase para resaltar el dot del carousel-item activo
 }
