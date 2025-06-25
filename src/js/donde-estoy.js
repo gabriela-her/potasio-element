4d7198da483cdc5898d99f1025d378b91123bb5f
irinatiron <irinatirongiurgiu@gmail.com>
2025-06-25 22:18:53 +0200

    fixed responsive nav

--- src/js/donde-estoy.js
+++ src/js/donde-estoy.js
@@ -1,13 +1,3 @@
- // Responsive Nav
- function responsiveNav() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-
 // Carousel
 
 let slideIndex = 1;
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,16 @@
  // Responsive Nav
  function responsiveNav() {
-            var x = document.getElementById("myTopnav");
+            let x = document.getElementById("myTopnav");
             if (x.className === "topnav") {
                 x.className += " responsive";
             } else {
                 x.className = "topnav";
             }
         }
+document.getElementById("que-soy-li-link").style.backgroundColor="rgba(250, 222, 82, 0.5)";
+document.getElementById("donde-estoy-li-link").style.backgroundColor="rgba(201, 227, 172, 0.5)";
+document.getElementById("mi-importancia-li-link").style.backgroundColor="rgba(97, 179, 35, 0.5)";
+document.getElementById("curiosidades-li-link").style.backgroundColor="rgba(67, 100, 54, 0.5)";
+document.getElementById("actualidad-li-link").style.backgroundColor="rgba(248, 140, 140, 0.5)";
+document.getElementById("creadoras-li-link").style.backgroundColor="rgba(250, 222, 82, 0.5)";
+
