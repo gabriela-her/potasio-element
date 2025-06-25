345f896c13178d30931d40e9f0ccf8e5349988b4
irinatiron <irinatirongiurgiu@gmail.com>
2025-06-25 23:25:47 +0200

    added eventListener to fa fa-bars

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,14 @@ document.getElementById("curiosidades-li-link").style.backgroundColor="rgba(67, 
 document.getElementById("actualidad-li-link").style.backgroundColor="rgba(248, 140, 140, 0.5)";
 document.getElementById("creadoras-li-link").style.backgroundColor="rgba(250, 222, 82, 0.5)";
 
+
+
+const rotateBars = document.getElementById("fa-bars-menu");
+rotateBars.addEventListener("click", function() {
+    const openMenu = document.getElementById("openMenu");
+    if (openMenu.style.transform === "rotate(90deg)") {
+        openMenu.style.transform = "rotate(180deg)";
+    } else {
+        openMenu.style.transform = "rotate(90deg)";
+    }
+});
