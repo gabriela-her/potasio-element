6bb4b325fe3782132dce08c953cfa1fcb2e32634
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-06-26 12:12:19 +0200

    actualizacion nav

--- src/js/curiosidades.js
+++ src/js/curiosidades.js
@@ -1,29 +1,86 @@
-const cards = document.querySelectorAll(".card");
-const modal = document.getElementById("modal");
-const modalText = document.getElementById("modal-text");
-const closeModalBtn = document.getElementById("cerrar-modal");
+document.addEventListener("DOMContentLoaded", () => {
+    const cards = document.querySelectorAll(".card");
+    const modal = document.getElementById("modal");
+    const modalTitle = document.getElementById("modal-title");
+    const modalText = document.getElementById("modal-text");
+    const closeModalBtn = document.getElementById("cerrar-modal");
+    const correctSound = new Audio("../public/sounds/potasiosound.mp3");
+    const incorrectSound = new Audio("../public/sounds/error.mp3");
+    incorrectSound.load();
+    const reiniciarButton = document.getElementById("reiniciar-button");
+    const verMemeBtn = document.getElementById("ver-meme-btn");
+    const videoModal = document.getElementById("video-modal");
+    const cerrarVideo = document.getElementById("cerrar-video");
+    const video = document.getElementById("potasio-video");
 
-cards.forEach(card => {
-    card.addEventListener("click", () => {
-    const isTrue = card.getAttribute("data-truth") === "true";
+    verMemeBtn.addEventListener("click", () => {
+        videoModal.style.display = "block";
+        video.currentTime = 0;
+        video.play();
+    });
 
-    if (isTrue) {
-        card.classList.add("correct");
-        card.computedStyleMap.borderColor =" #4caf50" ;
-    } else {
-        const correctInfo = card.getAttribute("data-correct");
-        modalText.textContent = correctInfo;
-        modal.style.display = "flex";
-        card.classList.add("incorrect");
-        card.style.borderColor = "#f44336" ;
-    }
-  });  
-});
-closeModalBtn.addEventListener("click", () => { 
-    modal.style.display = "none" ;
-});
-window.addEventListener("click", (event) => {
+    cerrarVideo.addEventListener("click", () => {
+        videoModal.style.display = "none";
+        video.pause();
+    });
+
+    window.addEventListener("click", (e) => {
+        if (e.target === videoModal) {
+            videoModal.style.display = "none";
+             video.pause();
+         }
+  });
+
+    reiniciarButton.addEventListener("click", () => {
+        cards.forEach(card => {
+            card.classList.remove("correct", "incorrect");
+            card.style.borderColor = "";
+        });
+        modal.style.display = "none";
+    });
+
+    cards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            card.classList.add("clicked");
+            setTimeout(() => {
+             card.classList.remove("clicked");         
+            }, 300);
+
+             const isTrue = card.getAttribute("data-truth") === "true";
+             const isExplosionCard = card.textContent.includes("reacciona con el agua");
+
+            if (isTrue) {
+             card.classList.add("correct");
+             card.style.borderColor =" #4caf50" ;
+             modalTitle.textContent = "¡CORRECTO!";
+             modalText.textContent = "¡Bien hecho!";
+             console.log("Ahora tiene que sonar")
+            modal.style.display = "flex";
+            correctSound.currentTime = 0;
+            correctSound.play();
+            confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+            });
+             } else {
+            const correctInfo = card.getAttribute("data-correct");
+            card.classList.add("incorrect");
+            modalTitle.textContent = "¡Incorrecto!";
+            modalText.textContent = correctInfo;
+            modal.style.display = "flex";
+            card.style.borderColor = "#f44336" ;
+            incorrectSound.currentTime = 0;
+            incorrectSound.play();
+        }
+     });  
+    });
+    closeModalBtn.addEventListener("click", ()   => { 
+    modal.style.display = "none";
+    });
+    window.addEventListener("click", (event) => {
     if (event.target === modal) {
     modal.style.display = "none" ;
-}
+        }
+    });
 });
